b01b4783dfa3f9a7badab6df040c79e872ea02ee
interkosmos <pengel@hs-nb.de>
2018-01-29 19:53:45 +0100

    Added mock-up.

---
+++ src/apps/settings/app.js
@@ -0,0 +1,8 @@
+/**
+ * @file      The settings page.
+ * @author    Philipp Engel
+ * @license   BSD-2-Clause
+ * @copyright Hochschule Neubrandenburg - University of Applied Sciences, 2018
+ * @see       {@link https://github.com/dabamos/openadms-ui/}
+ */
+
